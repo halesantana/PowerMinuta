6b9a5f52674340abec56f01e54d2226f80a6700e
Hernan Santana <hernan@focoosin.com>
2016-12-15 23:30:34 -0300

    Creación de tablas y registros en la base de datos

--- tesis/powerMinuta/admin.py
+++ tesis/powerMinuta/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Nutricionista, Paciente, Ficha, Clasificacion, Alimento, Minuta
+
+admin.site.register(Nutricionista)
+admin.site.register(Paciente)
+admin.site.register(Ficha)
+admin.site.register(Clasificacion)
+admin.site.register(Alimento)
+admin.site.register(Minuta)
